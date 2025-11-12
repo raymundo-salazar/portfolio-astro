312a4220ee4ff40c4b7f9ee0ab71ac7beee556f9
Raymundo Salazar <rsalazar@vittilog.com>
2025-11-11 20:29:03 -0600

    feat(Hero): implement HeroSocialIcon component for social links rendering feat(Hero): create constants for social links to improve maintainability

---
+++ src/components/organisms/Hero/constants.ts
@@ -0,0 +1,21 @@
+import { SiGithub } from "react-icons/si"
+import { SlSocialLinkedin } from "react-icons/sl"
+import { Mail } from "@lucide/astro"
+
+export const SOCIAL_LINKS = [
+	{
+		name: "Email",
+		url: `mailto:${import.meta.env.PUBLIC_EMAIL_ADDRESS}`,
+		icon: Mail,
+	},
+	{
+		name: "GitHub",
+		url: `https://github.com/${import.meta.env.PUBLIC_GITHUB_USERNAME}`,
+		icon: SiGithub,
+	},
+	{
+		name: "LinkedIn",
+		url: `https://www.linkedin.com/in/${import.meta.env.PUBLIC_LINKEDIN_USERNAME}`,
+		icon: SlSocialLinkedin,
+	},
+]
